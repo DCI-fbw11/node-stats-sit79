e57b52009965268d9e496d63a806b6306048d9a8
sit79 <stefan.tolksdorf@gmail.com>
2019-03-31 18:53:01 +0200

    help text amended

--- cmds/help.js
+++ cmds/help.js
@@ -2,13 +2,14 @@ const menus = {
   main: `
     node-stats [command] <options>
 
-    scan ............... scan a folder and its subfolders for node_modules
+    scan ............... scan a folder and all its subfolders for node_modules
     help ............... show help menu for a command\n`,
 
   scan: `
     node-stats scan <options>
 
-    path ............. scan a specific folder\n`
+    path ............. scan a specific folder\n
+    If no path is specified, the current directory will be scanned.\n`
 };
 
 module.exports = args => {
